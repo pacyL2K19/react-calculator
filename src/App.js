8112de4af74eba4ae17c1f0eaae736390ab75739
Pacifique LINJANJA <pacilinja1@icloud.com>
2021-07-01 16:37:16 +0100

    Configure and fix eslint issues

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import React from "react";
+import React from 'react';
 
-const App = () => {
-  return (
-    <div>
-      <h2>React Calculator</h2>
-    </div>
-  );
-};
+const App = () => (
+  <div>
+    <h2>React Calculator</h2>
+  </div>
+);
 
 export default App;
