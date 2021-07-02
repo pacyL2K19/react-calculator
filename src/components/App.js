f7ee757b516e93b848959d9b76b582dc72764ba3
Pacifique LINJANJA <pacilinja1@icloud.com>
2021-07-02 18:22:44 +0100

    Update the App component

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ButtonPanel from './ButtonPanel';
+import Display from './Display';
+
+function App() {
+  return (
+    <>
+        <Display />
+        <ButtonPanel />
+    </>
+  );
+}
+
+export default App;
