861fb1c85f7460527aee2e376852b2949ae98f12
Pacifique LINJANJA <pacilinja1@icloud.com>
2021-07-02 18:49:48 +0100

    Fix the proptypes issue

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Button = () => (
-  <div>{this.props.name}</div>
-
+const Button = ({ name }) => (
+  <div>{name}</div>
 );
 
 Button.propTypes = {
-  name: PropTypes.string,
+  name: PropTypes.string.isRequired,
 };
 
 export default Button;
