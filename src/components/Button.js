85f215b64e998e83c79d04013e364a9d07735fda
Pacifique LINJANJA <pacilinja1@icloud.com>
2021-07-07 07:38:37 +0100

    Fix the button component

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Button = ({ name }) => (
-  <div>{name}</div>
-);
+const Button = ({ name, clickHandler }) => {
+  const handleClick = (name) => {
+    clickHandler(name);
+  };
+  return (
+    name === '=' ? <div className="button button-equal" onClick={() => handleClick(name)}><span className="text">{name}</span></div> : <div className="button" onClick={() => handleClick(name)}><span className="text">{name}</span></div>
+  );
+};
 
 Button.propTypes = {
   name: PropTypes.string.isRequired,
+  clickHandler: PropTypes.func.isRequired,
 };
 
 export default Button;
