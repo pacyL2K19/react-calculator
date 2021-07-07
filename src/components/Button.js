ecbdbeda71f7b4879d61a0f5e9bfccc647b855d1
Pacifique LINJANJA <pacilinja1@icloud.com>
2021-07-07 09:14:07 +0100

    Update the button symbol designs

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,11 +2,12 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Button = ({ name, clickHandler }) => {
+  const symbols = ['AC', '+/-', '%', '/', 'X', '-', '+'];
   const handleClick = (name) => {
     clickHandler(name);
   };
   return (
-    name === '=' ? <div className="button button-equal" onClick={() => handleClick(name)} onKeyDown={() => handleClick(name)} tabIndex={0} role="button"><span className="text">{name}</span></div> : <div className="button" onClick={() => handleClick(name)} onKeyDown={() => handleClick(name)} tabIndex={0} role="button"><span className="text">{name}</span></div>
+    name === '=' ? <div className="button button-equal" onClick={() => handleClick(name)} onKeyDown={() => handleClick(name)} tabIndex={0} role="button"><span className="text">{name}</span></div> : <div className={symbols.includes(name) ? 'symbol' : 'button'} onClick={() => handleClick(name)} onKeyDown={() => handleClick(name)} tabIndex={0} role="button"><span className="text">{name}</span></div>
   );
 };
 
