96cf206112e2cbf68f940c843a77e204af1dafe2
Pacifique LINJANJA <pacilinja1@icloud.com>
2021-07-07 08:27:38 +0100

    Clean the code

--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -42,35 +42,6 @@ const ButtonPanel = (props) => {
           ))
         }
       </div>
-      {/* <div>
-        <Button name="AC" clickHandler={handleClick} />
-        <Button name="+/-" clickHandler={handleClick} />
-        <Button name="%" clickHandler={handleClick} />
-        <Button name="/" clickHandler={handleClick} />
-      </div> */}
-      {/* <div>
-        <Button name="7" clickHandler={handleClick} />
-        <Button name="8" clickHandler={handleClick} />
-        <Button name="9" clickHandler={handleClick} />
-        <Button name="X" clickHandler={handleClick} />
-      </div>
-      <div>
-        <Button name="4" clickHandler={handleClick} />
-        <Button name="5" clickHandler={handleClick} />
-        <Button name="6" clickHandler={handleClick} />
-        <Button name="−" clickHandler={handleClick} />
-      </div>
-      <div>
-        <Button name="1" clickHandler={handleClick} />
-        <Button name="2" clickHandler={handleClick} />
-        <Button name="3" clickHandler={handleClick} />
-        <Button name="+" clickHandler={handleClick} />
-      </div>
-      <div>
-        <Button name="0" clickHandler={handleClick} />
-        <Button name="." clickHandler={handleClick} />
-        <Button name="=" clickHandler={handleClick} />
-      </div> */}
     </div>
   );
 };
