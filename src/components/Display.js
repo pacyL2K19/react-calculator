3d79e28c99c18267f29a9bc09b860ea793ab2736
Pacifique LINJANJA <pacilinja1@icloud.com>
2021-07-07 07:42:28 +0100

    Minor update on the display view

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Display = ({ result }) => (
-  <div>
-    {result}
-  </div>
-);
+class Display extends React.Component {
+  // eslint-disable-next-line no-useless-constructor
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const { result } = this.props;
+    return (
+      <div className="element text">
+        {result}
+      </div>
+    );
+  }
+}
 
 Display.propTypes = {
   result: PropTypes.string,
