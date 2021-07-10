7817fa40a4c33ec3c670e6d29baadd8e0fde6b4a
Pacifique LINJANJA <pacilinja1@icloud.com>
2021-07-11 00:05:50 +0100

    Update the button component

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,23 +1,31 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Button = ({ name, clickHandler }) => {
-  const symbols = ['AC', '+/-', '%', '/', 'X', '−', '+'];
+const Button = ({
+  name, clickHandler, color, wide,
+}) => {
   const handleClick = (name) => {
     clickHandler(name);
   };
-  const addClass = (name) => {
-    if (name === '0') return 'button-zero';
+  const addClass = () => {
+    if (wide) return 'button-zero';
     return '';
   };
   return (
-    name === '=' ? <div className="button" onClick={() => handleClick(name)} onKeyDown={() => handleClick(name)} tabIndex={0} role="button"><span className="text">{name}</span></div> : <div className={symbols.includes(name) ? 'symbol' : `button ${addClass(name)}`} onClick={() => handleClick(name)} onKeyDown={() => handleClick(name)} tabIndex={0} role="button"><span className="text">{name}</span></div>
+    name === '=' ? <div className="button" onClick={() => handleClick(name)} onKeyDown={() => handleClick(name)} tabIndex={0} role="button"><span className="text">{name}</span></div> : <div className={color ? 'symbol' : `button ${addClass()}`} onClick={() => handleClick(name)} onKeyDown={() => handleClick(name)} tabIndex={0} role="button"><span className="text">{name}</span></div>
   );
+};
+
+Button.defaultProps = {
+  wide: false,
+  color: '',
 };
 
 Button.propTypes = {
   name: PropTypes.string.isRequired,
   clickHandler: PropTypes.func.isRequired,
+  color: PropTypes.string,
+  wide: PropTypes.bool,
 };
 
 export default Button;
