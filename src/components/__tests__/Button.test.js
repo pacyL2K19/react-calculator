5aedd9a10d9f1f1ef76785879dbac60dae728043
Pacifique LINJANJA <pacilinja1@icloud.com>
2021-07-12 23:13:01 +0100

    Write tests for the buttonPannel component

--- src/components/__tests__/Button.test.js
+++ src/components/__tests__/Button.test.js
@@ -5,13 +5,11 @@ import Button from '../Button';
 describe('The button component', () => {
   let wrapper;
   it('Has the button class', () => {
-    wrapper = shallow(<Button name="1" />);
+    wrapper = shallow(<Button name="1" clickHandler={() => {}} />);
     expect(wrapper.find('.button').length).toEqual(1);
   });
-  //   it('Has the button-zero class when 0 is passed as name prop', () => {
-
-  //   });
-  //   describe("The click event", () => {
-
-//   })
+  it('Displays the name in the button component', () => {
+    wrapper = shallow(<Button name="+" clickHandler={() => {}} />);
+    expect(wrapper.text()).toBe("+");
+  });
 });
---
+++ src/components/__tests__/ButtonPannel.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { shallow, mount } from 'enzyme';
+import ButtonPannel from '../ButtonPanel';
+
+describe('The buttonPannel component', () => {
+  let wrapper;
+  it('Has at least 19 button components displayed', () => {
+    wrapper = shallow(<ButtonPannel clickHandler={() => {}} />);
+    expect(wrapper.find('Button').length).toBeGreaterThanOrEqual(19);
+  });
+});
