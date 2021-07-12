879b1c971c72e776531b451e94b05da0a5b94d58
Pacifique LINJANJA <pacilinja1@icloud.com>
2021-07-12 23:14:21 +0100

    Remove unused vars

--- src/components/__tests__/ButtonPannel.test.js
+++ src/components/__tests__/ButtonPannel.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { shallow, mount } from 'enzyme';
+import { shallow } from 'enzyme';
 import ButtonPannel from '../ButtonPanel';
 
 describe('The buttonPannel component', () => {
