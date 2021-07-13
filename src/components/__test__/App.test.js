dbe408c18e35e473fb88fd271b4f970dc68e7368
Pacifique LINJANJA <pacilinja1@icloud.com>
2021-07-13 19:06:45 +0100

    Fix eslint issues

--- src/components/__test__/App.test.js
+++ src/components/__test__/App.test.js
@@ -3,81 +3,70 @@ import { render } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import App from '../App';
 
-// Test rendering Home components
-it('Should render App title', () => {
-  const { getByText } = render(<App />);
-  const headerText = getByText(/Denis React Calculator/);
-  expect(headerText).toBeInTheDocument();
-});
+test('App component', () => {
+  it('Should render AC', () => {
+    const { getByText } = render(<App />);
+    const btnName = getByText(/AC/);
+    expect(btnName).toBeInTheDocument();
+  });
 
-it('Should render AC', () => {
-  const { getByText } = render(<App />);
-  const btnName = getByText(/AC/);
-  expect(btnName).toBeInTheDocument();
-});
+  it('Should render =', () => {
+    const { getByText } = render(<App />);
+    const btnName = getByText(/=/);
+    expect(btnName).toBeInTheDocument();
+  });
 
-it('Should render =', () => {
-  const { getByText } = render(<App />);
-  const btnName = getByText(/=/);
-  expect(btnName).toBeInTheDocument();
-});
+  it('Should render calculator button 1', () => {
+    const { getByText } = render(<App />);
+    const btnName = getByText(/1/);
+    expect(btnName).toBeInTheDocument();
+  });
 
-it('Should render calculator button 1', () => {
-  const { getByText } = render(<App />);
-  const btnName = getByText(/1/);
-  expect(btnName).toBeInTheDocument();
-});
+  it('Should render calculator button 2', () => {
+    const { getByText } = render(<App />);
+    const btnName = getByText(/2/);
+    expect(btnName).toBeInTheDocument();
+  });
 
-it('Should render calculator button 2', () => {
-  const { getByText } = render(<App />);
-  const btnName = getByText(/2/);
-  expect(btnName).toBeInTheDocument();
-});
+  it('Should render calculator button 3', () => {
+    const { getByText } = render(<App />);
+    const btnName = getByText(/3/);
+    expect(btnName).toBeInTheDocument();
+  });
 
-it('Should render calculator button 3', () => {
-  const { getByText } = render(<App />);
-  const btnName = getByText(/3/);
-  expect(btnName).toBeInTheDocument();
-});
+  it('Should render calculator button 4', () => {
+    const { getByText } = render(<App />);
+    const btnName = getByText(/4/);
+    expect(btnName).toBeInTheDocument();
+  });
 
-it('Should render calculator button 4', () => {
-  const { getByText } = render(<App />);
-  const btnName = getByText(/4/);
-  expect(btnName).toBeInTheDocument();
-});
+  it('Should render calculator button 5', () => {
+    const { getByText } = render(<App />);
+    const btnName = getByText(/5/);
+    expect(btnName).toBeInTheDocument();
+  });
 
-it('Should render calculator button 5', () => {
-  const { getByText } = render(<App />);
-  const btnName = getByText(/5/);
-  expect(btnName).toBeInTheDocument();
-});
+  it('Should render calculator button 6', () => {
+    const { getByText } = render(<App />);
+    const btnName = getByText(/6/);
+    expect(btnName).toBeInTheDocument();
+  });
 
-it('Should render calculator button 6', () => {
-  const { getByText } = render(<App />);
-  const btnName = getByText(/6/);
-  expect(btnName).toBeInTheDocument();
-});
+  it('Should render calculator button 7', () => {
+    const { getByText } = render(<App />);
+    const btnName = getByText(/7/);
+    expect(btnName).toBeInTheDocument();
+  });
 
-it('Should render calculator button 7', () => {
-  const { getByText } = render(<App />);
-  const btnName = getByText(/7/);
-  expect(btnName).toBeInTheDocument();
-});
+  it('Should render calculator button 8', () => {
+    const { getByText } = render(<App />);
+    const btnName = getByText(/8/);
+    expect(btnName).toBeInTheDocument();
+  });
 
-it('Should render calculator button 8', () => {
-  const { getByText } = render(<App />);
-  const btnName = getByText(/8/);
-  expect(btnName).toBeInTheDocument();
-});
-
-it('Should render calculator button 8', () => {
-  const { getByText } = render(<App />);
-  const btnName = getByText(/9/);
-  expect(btnName).toBeInTheDocument();
-});
-
-it('Should render calculator button 0', () => {
-  const { getByText } = render(<App />);
-  const btnName = getByText(/0/);
-  expect(btnName).toBeInTheDocument();
+  it('Should render calculator button 8', () => {
+    const { getByText } = render(<App />);
+    const btnName = getByText(/9/);
+    expect(btnName).toBeInTheDocument();
+  });
 });
--- src/screens/__tests__/Home-test.js
+++ src/screens/__tests__/Home-test.js
@@ -7,3 +7,21 @@ describe('Home page', () => {
     expect(tree).toMatchSnapshot();
   });
 });
+
+// import React from 'react';
+// import { render } from '@testing-library/react';
+// import '@testing-library/jest-dom';
+// import Home from '../components/Home';
+
+// // Test rendering Home components
+// it('Should renders Home title', () => {
+//   const { getByText } = render(<Home />);
+//   const headerText = getByText(/Welcome to Math Magician/);
+//   expect(headerText).toBeInTheDocument();
+// });
+
+// it('Should renders Home body', () => {
+//   const { getByText } = render(<Home />);
+//   const headerText = getByText(/Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua./);
+//   expect(headerText).toBeInTheDocument();
+// });
