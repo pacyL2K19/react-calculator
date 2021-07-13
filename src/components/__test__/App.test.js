06d56faa6cd2456700121015ecfa4543cd6d3a12
Pacifique LINJANJA <pacilinja1@icloud.com>
2021-07-13 18:58:50 +0100

    Implement the test of the App component

--- src/components/__test__/App.test.js
+++ src/components/__test__/App.test.js
@@ -1,24 +1,83 @@
 import React from 'react';
-import { shallow, mount } from 'enzyme';
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom';
 import App from '../App';
 
-describe('The App component of the Calculator screen', () => {
-  let wrapper;
-  it('Has the one Display component', () => {
-    wrapper = shallow(<App />);
-    expect(wrapper.find('Display').length).toEqual(1);
-  });
-  it('Has at least one ButtonPanel component', () => {
-    wrapper = shallow(<App />);
-    expect(wrapper.find('ButtonPanel').length).toBe(1);
-  });
-  describe('Click events on buttons, display text in the board', () => {
-    beforeEach(() => {
-      wrapper = mount(<App />);
-      return null;
-    });
-    it('Always starts with 0 displayed in the board', () => {
-      expect(wrapper.find('Display').text()).toBe('0');
-    });
-  });
+// Test rendering Home components
+it('Should render App title', () => {
+  const { getByText } = render(<App />);
+  const headerText = getByText(/Denis React Calculator/);
+  expect(headerText).toBeInTheDocument();
+});
+
+it('Should render AC', () => {
+  const { getByText } = render(<App />);
+  const btnName = getByText(/AC/);
+  expect(btnName).toBeInTheDocument();
+});
+
+it('Should render =', () => {
+  const { getByText } = render(<App />);
+  const btnName = getByText(/=/);
+  expect(btnName).toBeInTheDocument();
+});
+
+it('Should render calculator button 1', () => {
+  const { getByText } = render(<App />);
+  const btnName = getByText(/1/);
+  expect(btnName).toBeInTheDocument();
+});
+
+it('Should render calculator button 2', () => {
+  const { getByText } = render(<App />);
+  const btnName = getByText(/2/);
+  expect(btnName).toBeInTheDocument();
+});
+
+it('Should render calculator button 3', () => {
+  const { getByText } = render(<App />);
+  const btnName = getByText(/3/);
+  expect(btnName).toBeInTheDocument();
+});
+
+it('Should render calculator button 4', () => {
+  const { getByText } = render(<App />);
+  const btnName = getByText(/4/);
+  expect(btnName).toBeInTheDocument();
+});
+
+it('Should render calculator button 5', () => {
+  const { getByText } = render(<App />);
+  const btnName = getByText(/5/);
+  expect(btnName).toBeInTheDocument();
+});
+
+it('Should render calculator button 6', () => {
+  const { getByText } = render(<App />);
+  const btnName = getByText(/6/);
+  expect(btnName).toBeInTheDocument();
+});
+
+it('Should render calculator button 7', () => {
+  const { getByText } = render(<App />);
+  const btnName = getByText(/7/);
+  expect(btnName).toBeInTheDocument();
+});
+
+it('Should render calculator button 8', () => {
+  const { getByText } = render(<App />);
+  const btnName = getByText(/8/);
+  expect(btnName).toBeInTheDocument();
+});
+
+it('Should render calculator button 8', () => {
+  const { getByText } = render(<App />);
+  const btnName = getByText(/9/);
+  expect(btnName).toBeInTheDocument();
+});
+
+it('Should render calculator button 0', () => {
+  const { getByText } = render(<App />);
+  const btnName = getByText(/0/);
+  expect(btnName).toBeInTheDocument();
 });
