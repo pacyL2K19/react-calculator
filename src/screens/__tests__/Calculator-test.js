56bc9b7dff1a72e18a978f17a669c3e32f34076c
Pacifique LINJANJA <pacilinja1@icloud.com>
2021-07-14 02:06:36 +0100

    Replace snapshots with rtl snapshot

--- src/screens/__tests__/Calculator-test.js
+++ src/screens/__tests__/Calculator-test.js
@@ -1,9 +1,10 @@
-import renderer from 'react-test-renderer';
+import React from 'react'
+import { render } from '@testing-library/react';
 import Calculator from '../Calculator';
 
 describe('Calculator page', () => {
   it('should match the snapshot', () => {
-    const tree = renderer.create(<Calculator />);
-    expect(tree).toMatchSnapshot();
+    const { asFragment } = render(<Calculator />);
+    expect(asFragment(<Calculator/>)).toMatchSnapshot();
   });
 });
