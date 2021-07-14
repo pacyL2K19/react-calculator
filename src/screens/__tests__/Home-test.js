23b64c0ba6b22f409cb6bdf9f7a32bdce6baced1
Pacifique LINJANJA <pacilinja1@icloud.com>
2021-07-14 02:09:29 +0100

    Fix linter issues

--- src/screens/__tests__/Home-test.js
+++ src/screens/__tests__/Home-test.js
@@ -1,10 +1,10 @@
-import React from 'react'
+import React from 'react';
 import { render } from '@testing-library/react';
 import Home from '../Home';
 
 describe('Snapshot Home page', () => {
   it('should match the snapshot', () => {
-    const { asFragment } = render(<Home />)
-    expect(asFragment(<Home/>)).toMatchSnapshot();
+    const { asFragment } = render(<Home />);
+    expect(asFragment(<Home />)).toMatchSnapshot();
   });
 });
